29674dc068bf8b47c462a26b22ff1d5b6bd59e72
Gabriel Almeida <obiellalm@gmail.com>
2021-12-06 11:51:52 -0300

    Update Final 06/12
    Update Final 06/12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,8 @@
     <link rel="stylesheet" href="/styles/list.css">
 
     <div class="container">
-
-        <table class="table table-stripped">
-            <thead>
+        <table class="table table-striped">
+            <thead >
                 <tr>
                     <th scope=col>Nome</th>
                     <th scope=col>Data de Nascimento</th>
@@ -16,56 +15,57 @@
                     <th scope=col>Alterar</th>
                     <th scope=col>Excluir</th>
                 </tr>
+
                 <tbody>
+                    @foreach ($users as $user)   
                     <tr>
-                      <td>
-                        @foreach ($users as $user)
-                            <p>{{ $user->name }}</p>
-                        @endforeach
-                      </td>
-
-                      <td>
-                        @foreach ($users as $user)
-                            <p>{{ date('d/m/Y', strtotime($user->birth)) }}</p>
-                        @endforeach
-                      </td>
-
-                      <td>
-                        @foreach ($users as $user)
-                            <p>{{ $user->gender }}</p>
-                        @endforeach
-                      </td>
-
-                      <td>
-                            @foreach ($users as $user)
-                                <p>{{ $user->cpf }}</p>
-                            @endforeach
-                      </td>
-
-                      <td>
-                            @foreach ($users as $user)
-                                <p><a href="/info/{{$user->id}}" class="btn btn-outline-info">Mais info <i class="fas fa-plus icon"></i></a></p>  
-                            @endforeach
-                      </td>
-
-                      <td>
-                        @foreach ($users as $user)
-                            <p><a href="/edit/{{$user->id}}" class="btn btn-outline-warning">Alterar <i class="fas fa-edit icon"></i></a></p>
-                        @endforeach
-                      </td>
-                      
-                      <td>
-                        @foreach ($users as $user)
+                        <td>
+                            <span>
+                                {{ $user->name }}
+                            </span>
+                        </td>
+                        
+                        <td>
+                            <span>
+                                {{ date('d/m/Y', strtotime($user->birth)) }}
+                            </span>
+                        </td>
+                        
+                        <td>
+                            <span>
+                                {{ $user->gender }}
+                            </span>
+                        </td>
+                        
+                        <td>
+                            <span>
+                                {{ $user->cpf }}
+                            </span>
+                        </td>
+                        
+                        <td>
+                            <span>
+                                <a href="/info/{{$user->id}}" class="btn btn-outline-info">Mais info <i class="fas fa-plus icon"></i></a>
+                            </span>  
+                        </td>
+                        
+                        <td>
+                            <span>
+                                <a href="/edit/{{$user->id}}" class="btn btn-outline-warning">Alterar <i class="fas fa-edit icon"></i></a>
+                            </span>
+                        </td>
+                        
+                        <td>
                             <form action="/{{$user->id}}" method="post">
                                 @csrf
                                 @method('DELETE')
                                 <button class="btn btn-danger" type="submit"><i class="fas fa-user-times"></i></button>
                             </form>
-                        @endforeach
-                      </td>
+                        </td>
                     </tr>
+                    @endforeach
                 </tbody>
             </thead>
         </table>
     </div>
-@endsection
+    @endsection
